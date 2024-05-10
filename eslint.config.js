ac7f37fe27f6e45423bc8fe5807b957b08eb41d9
Martin Böschen <martin.boeschen@gmail.com>
2024-05-10 16:21:57 +0200

    added linting

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ["/example_app_react/dist/assets"]
+  },
+  pluginJs.configs.recommended,
+];
